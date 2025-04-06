272db356e3b5ecbd508f3af3a10a92619b93faa2
Ícaro Teodoro <icaro.teodoro84@icloud.com>
2025-04-06 15:00:39 -0300

    create store context

---
+++ src/services/storeService.ts
@@ -0,0 +1,155 @@
+import api from "@/lib/axios";
+
+import { destroyCookie } from "nookies";
+import { getEmailByToken, saveRefreshToken, saveToken } from "./tokenService";
+import { redirect } from 'next/navigation'
+import { StoreStatus } from "@/context/StoreContext";
+
+
+
+
+interface iCreateStore {
+  name: string;
+  document: string;
+  email:string;
+  password: string;
+}
+
+export interface iStore {
+  id:string,
+  name: string,
+  document: string,
+  email: string,
+  description: string,
+  category: string,
+  statusLive: StoreStatus
+}
+
+export interface iAddress {
+  id:string,
+  cep: string,
+  number:string,
+  street:string,
+  complement: string,
+  neighborhood:string,
+  city: string,
+  country: string
+}
+
+export async function createStore(data: iCreateStore) {
+  try {
+      const response = await api.post('/auth/store/register', {
+          name: data.name,
+          document: data.document,
+          email: data.email,
+          password: data.password,
+          address: {} // Garante que address não será undefined
+      });
+
+      console.log("Loja cadastrada com sucesso:", response.data);
+      saveToken(response.data.token);
+      saveRefreshToken('refreshToken', response.data.refresh_token);
+      redirect('/home')
+  } catch (error: any) {
+      console.error("Erro ao cadastrar a loja:", error.response?.data || error.message);
+      throw error; // Propaga o erro para que a chamada possa tratá-lo
+  }
+}
+
+export async function loginStore(email: string, password: string) {
+  try {
+    const response = await api.post("/auth/store/login", {
+      email,
+      password,
+    });
+
+    saveToken(response.data.token);
+    saveRefreshToken(response.data.refreshToken);
+  } catch (error: any) {
+    console.error(
+      "Erro no login:",
+      error.response?.data || error.message
+    );
+    throw error;
+  }
+  window.location.href = "/home";
+}
+
+export async function getStore():Promise<iStore> {
+  const email = getEmailByToken();
+
+  const store = await api.get(`/store/${email}`);
+
+  return store.data;
+}
+
+
+
+export async function changeStoreStatus (newStatus: any) {
+  const email = getEmailByToken();
+
+  try {
+    await api.put('/store/update-status', {
+      email,
+      status: newStatus
+    })
+    return true;
+  }catch(err) {
+    return false;
+  }
+
+}
+
+
+export async function updateStore(editValues:iStore) {
+  const email = getEmailByToken();
+
+  try {
+    await api.put(`/store/update/${email}`, {
+      name: editValues.name,
+      description:editValues.description,
+      category: editValues.category
+    })
+    return true;
+  }catch(err) {
+    return false;
+  }
+}
+
+
+export async function getStoreAddress() {
+  const email = getEmailByToken();
+
+  const address = await api.get(`/store/address/${email}`);
+
+  return address.data;
+
+}
+
+export async function updateStoreAddress(editValues:iAddress){
+  const email = getEmailByToken();
+
+  try {
+    await api.put(`/address/store/update/${editValues.id}`, {
+      cep: editValues.cep,
+      number: editValues.number,
+      street: editValues.street,
+      neighborhood: editValues.neighborhood,
+      city: editValues.city,
+      country: editValues.country,
+      complement: editValues.complement
+    })
+    return true;
+  }catch(err) {
+    return false;
+  }
+}
+
+
+export async function logout(){
+  if(await changeStoreStatus("CLOSED")){
+    destroyCookie(null, 'token')
+    destroyCookie(null, 'refreshToken')
+    window.location.href = '/';
+  }
+}
