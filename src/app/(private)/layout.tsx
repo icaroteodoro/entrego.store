000cd681282f8efb77f162d651678bfe8e397456
Ícaro Teodoro <icaro.teodoro84@icloud.com>
2025-04-22 19:43:32 -0300

    add toaster

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -2,20 +2,19 @@ import AppSidebar from "@/components/app-sidebar";
 import InternHeader from "@/components/intern-header";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { StoreProvider } from "@/context/store-context";
+import { Toaster } from "@/components/ui/sonner"
 
-
-export default function InternLayout(
-  { children }: Readonly<{ children: React.ReactNode }>
-) {
+export default function InternLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <StoreProvider>
       <SidebarProvider>
         <AppSidebar />
         <main className="w-full">
           <InternHeader />
-          <section className="p-3">
-            {children}
-          </section>
+          <section className="p-3">{children}</section>
+          <Toaster/>
         </main>
       </SidebarProvider>
     </StoreProvider>
